cc6f077cc7b2e613648b60a68d3ad4c843547843
xcarceco <xcarceco@gmail.com>
2022-02-21 23:15:34 +0100

    Initial commit

---
+++ Ejercicio2básicopython.py
@@ -0,0 +1,11 @@
+# En este ejercicio tendrás que crear un archivo
+# python donde tendrás que crear una variable
+# a la cual asignarás una cadena de texto y la imprimirás por consola.
+# Por otro lado, tendrás que modificar la variable y después imprimirla
+# por consola para ver la modificación de la variable.
+
+variable = 'Esto es una variable'
+print(variable)
+
+variable=variable.replace('o','u').replace('e','u').replace('E','U')
+print(variable)
